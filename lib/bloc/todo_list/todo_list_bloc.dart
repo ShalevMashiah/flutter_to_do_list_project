ea90964016241c6bd9bf821542ed9b227c0633fe
YF Software Development <yf.software.dev@gmail.com>
2025-12-01 09:04:06 +0200

    implement TodoListBloc with add, toggle, edit, delete logic

---
+++ lib/bloc/todo_list/todo_list_bloc.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'todo_list_event.dart';
+import 'todo_list_state.dart';
+
+class TodoListBloc extends Bloc<TodoListEvent, TodoListState> {
+  TodoListBloc() : super(const TodoListState()) {
+    
+    on<AddTodo>((event, emit) {
+      final updatedTodos = List.of(state.todos)..add(event.todo);
+      emit(state.copyWith(todos: updatedTodos));
+    });
+
+    on<ToggleTodo>((event, emit) {
+      final updatedTodos = state.todos.map((todo) {
+        if (todo.id == event.id) {
+          if (!todo.isDone) {
+            // Mark as completed + save completion time
+            return todo.copyWith(
+              isDone: true,
+              completedAt: DateTime.now(),
+            );
+          } else {
+            // Uncomplete the task
+            return todo.copyWith(
+              isDone: false,
+              completedAt: null,
+            );
+          }
+        }
+        return todo;
+      }).toList();
+
+      emit(state.copyWith(todos: updatedTodos));
+    });
+
+    on<DeleteTodo>((event, emit) {
+      final updatedTodos = state.todos.where((todo) => todo.id != event.id).toList();
+      emit(state.copyWith(todos: updatedTodos));
+    });
+
+    on<EditTodo>((event, emit) {
+      final updatedTodos = state.todos.map((todo) {
+        if (todo.id == event.id) {
+          return todo.copyWith(
+            title: event.newTitle,
+            description: event.newDescription,
+          );
+        }
+        return todo;
+      }).toList();
+
+      emit(state.copyWith(todos: updatedTodos));
+    });
+  }
+}
